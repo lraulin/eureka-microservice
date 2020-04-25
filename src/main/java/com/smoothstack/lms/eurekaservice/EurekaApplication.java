dabde4dbf350dc5519c1fc2049e5c1da9ddd4f09
timothy-breitenfeldt <timothy.breitenfeldt@smoothstack.com>
2020-04-25 14:57:08 -0400

    Refactored code to be consistent with other microservices.

--- src/main/java/com/smoothstack/lms/eurekaservice/EurekaApplication.java
+++ src/main/java/com/smoothstack/lms/eurekaservice/EurekaApplication.java
@@ -1,4 +1,4 @@
-package com.smoothstack.eureka.eureka;
+package com.smoothstack.lms.eurekaservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
